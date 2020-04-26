4e0c74f7a254369f0eafc7228ffb07a1db0a7207
LUUQUAN-PC <LUUQUAN-PC@192.168.56.1>
2020-04-26 23:07:51 +0700

    sd

---
+++ src/cataloog2/Cataloog2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cataloog2;
+
+/**
+ *
+ * @author LUUQUAN-PC
+ */
+public class Cataloog2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        System.out.print("Quan dep traia");
+    }
+    
+}
